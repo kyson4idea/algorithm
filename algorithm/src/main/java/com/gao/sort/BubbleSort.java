980c098fc9686954793585c606815c5fe0c24e9a
DESKTOP-QIA8S91\Gqijian <1753484293@qq.com>
2019-12-25 21:49:06 +0800

    -- update

--- algorithm/src/main/java/com/gao/sort/BubbleSort.java
+++ algorithm/src/main/java/com/gao/sort/BubbleSort.java
@@ -1,9 +1,45 @@
 package com.gao.sort;
 
 /**
+ * Bubble sort and related optimizations
  * @author gaoqijian
  * @create 2019-12-25 18:48
+ *
+ * @description 基本思路：通过比较排序序列前后，依次比较相邻元素的值，若发现`逆序`则交换
+ * 规则：
+ *  1、一共进行 length-1 次大循环
+ *  2、每一趟排序的次数逐渐减少
+ *  3、若某一趟无交换，可提前结束
  */
 public class BubbleSort {
+
+    public int[] bubbleSort(int[] arr){
+
+        boolean flag = false;
+        int tmp;
+
+        for(int i=0; i<arr.length-1; i++){
+
+            for(int j=0; j<arr.length-1-i; j++){
+
+                //发现 逆序 交换
+                if(arr[j] > arr[j+1]){
+                    tmp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = tmp;
+                    flag = true;
+                }
+            }
+            //若无交换 则跳出循环
+            if(!flag){
+                break;
+            }else {
+                flag = false;
+            }
+
+        }
+
+        return arr;
+    }
 
 }
--- algorithm/src/main/java/com/gao/sort/SelectSort.java
+++ algorithm/src/main/java/com/gao/sort/SelectSort.java
@@ -1,9 +1,37 @@
 package com.gao.sort;
 
 /**
+ *
  * @author gaoqijian
  * @create 2019-12-25 18:48
+ *
+ * @description 基本思路：第一次从arr[0]~arr[n-1]中选取最小值，与arr[0]交换，第二次同理从arr[1]~arr[n-1]中选最小，
+ * 通过n-1次交换，完成排序
  */
 public class SelectSort {
+
+    public int[] selectSort(int[] arr){
+
+        for (int i=0; i<arr.length-1; i++){
+
+            int minIndex = i;
+            int min = arr[i];
+
+            for(int j=0+i; j<arr.length; j++){
+
+                //找出 最小值与他的下标
+                if(min > arr[j]){
+                    min = arr[j];
+                    minIndex = j;
+                }
+            }
+
+            if(minIndex != i){
+                arr[minIndex] = arr[i];
+                arr[i] = min;
+            }
+        }
+        return arr;
+    }
 
 }
