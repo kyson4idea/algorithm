fa24622c71fe0e468a297854a969d17089c09978
Gqijian <1753484293@qq.com>
2020-11-05 10:18:32 +0800

    --update BinarySearch.java

--- algorithm/src/main/java/com/gao/search/BinarySearch.java
+++ algorithm/src/main/java/com/gao/search/BinarySearch.java
@@ -11,37 +11,10 @@ import java.util.List;
 public class BinarySearch {
 
     public static void main(String[] args) {
-
+        int[] arr = {-1,0,11,89,48,123,222};
+        binarySearchDemo(arr, 0, arr.length-1, 0);
+        System.out.println(binarySearchDemo(arr, 0, arr.length-1, 0));
     }
-
-    /**
-     *
-     * @param arr
-     * @param left 左边索引
-     * @param right 右边索引
-     * @param findVal 要查找的值
-     * @return 如果找到就返回下标，没找到 返回 -1
-     */
-    public static int binarySearchDemo(int[] arr, int left, int right, int findVal){
-
-        if(left > right){
-            return -1;
-        }
-
-        int mid = (left + right) / 2;
-        int midVal = arr[mid];
-
-        if(findVal > midVal){ //向右递归
-            return binarySearchDemo(arr, mid + 1, right, findVal);
-        }else if(findVal < midVal){
-            return binarySearchDemo(arr, left, mid - 1, findVal);
-        }else {
-            return mid;
-        }
-
-    }
-
-    //
 
     /**
      * 1、找到 mid 不要马上返回
@@ -98,4 +71,35 @@ public class BinarySearch {
         }
 
     }
+
+    /**
+     *
+     * @param arr
+     * @param left 左边索引
+     * @param right 右边索引
+     * @param findVal 要查找的值
+     * @return 如果找到就返回下标，没找到 返回 -1
+     */
+    public static int binarySearchDemo(int[] arr, int left, int right, int findVal){
+
+        if(left > right){
+            return -1;
+        }
+
+        int mid = (left + right) / 2;
+        int midVal = arr[mid];
+
+        if(findVal > midVal){ //向右递归
+            return binarySearchDemo(arr, mid + 1, right, findVal);
+        }else if(findVal < midVal){
+            return binarySearchDemo(arr, left, mid - 1, findVal);
+        }else {
+            return mid;
+        }
+
+    }
+
+    //
+
+
 }
