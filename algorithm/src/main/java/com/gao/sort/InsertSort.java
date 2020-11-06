53ce6a60be48059dcd4fe4046292ebf1b13a0d6c
Gqijian <1753484293@qq.com>
2020-11-06 15:46:23 +0800

    --update update insertSort

--- algorithm/src/main/java/com/gao/sort/InsertSort.java
+++ algorithm/src/main/java/com/gao/sort/InsertSort.java
@@ -1,5 +1,7 @@
 package com.gao.sort;
 
+import java.util.Arrays;
+
 /**
  * @author gaoqijian
  * @title Insert Sort
@@ -36,6 +38,57 @@ public class InsertSort {
                 arr[insertIndex + 1] = insertVal;
             }
         }
+    }
+
+    public static void stepOne(int[] arr) {
+        //第 1 轮 {101, 34, 119, 1}; => {34, 101, 119, 1}
+        //{101, 34, 119, 1}; => {101,101,119,1}
+        //定义待插入的数
+        int insertVal = arr[1];
+        int insertIndex = 1 - 1; //即 arr[1]的前面这个数的下标
+
+        //给 insertVal 找到插入的位置
+        //说明
+        //1. insertIndex >= 0 保证在给 insertVal 找插入位置，不越界
+        //2. insertVal < arr[insertIndex] 待插入的数，还没有找到插入位置
+        //3. 就需要将 arr[insertIndex] 后移
+        while (insertIndex >= 0 && insertVal < arr[insertIndex]) {
+            arr[insertIndex + 1] = arr[insertIndex];// arr[insertIndex]
+            insertIndex--;
+        }
+
+        //当退出 while 循环时，说明插入的位置找到, insertIndex + 1
+        //举例：理解不了，我们一会 debug
+        arr[insertIndex + 1] = insertVal;
+
+        System.out.println("第 1 轮插入");
+        System.out.println(Arrays.toString(arr));
+
+
+        //第 2 轮
+        insertVal = arr[2];
+        insertIndex = 2 - 1;
+        while (insertIndex >= 0 && insertVal < arr[insertIndex]) {
+            arr[insertIndex + 1] = arr[insertIndex];// arr[insertIndex]
+            insertIndex--;
+        }
+        arr[insertIndex + 1] = insertVal;
+        System.out.println("第 2 轮插入");
+        System.out.println(Arrays.toString(arr));
+
+
+        //第 3 轮
+        insertVal = arr[3];
+        insertIndex = 3 - 1;
+        while (insertIndex >= 0 && insertVal < arr[insertIndex]) {
+            arr[insertIndex + 1] = arr[insertIndex];// arr[insertIndex]
+            insertIndex--;
+        }
+
+
+        arr[insertIndex + 1] = insertVal;
+        System.out.println("第 3 轮插入");
+        System.out.println(Arrays.toString(arr));
     }
 
 }
