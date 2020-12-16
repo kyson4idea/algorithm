95e02ab4f0f7914fe101b2a632431edf23a1389a
Gqijian <1753484293@qq.com>
2020-12-16 17:58:29 +0800

    --update ShellSort.java

---
+++ algorithm/src/main/java/com/gao/exam/StringReverse.java
@@ -0,0 +1,57 @@
+package com.gao.exam;
+
+/**
+ * 将一个字符串进行反转。将字符串中指定部分进行 反转如 。比如“abcdefg”反 反
+ * 为 转为”abfedcg”
+ *
+ * @author gaoqijian
+ * @create 2020-12-16 16:54
+ */
+public class StringReverse {
+
+    /**
+     * 方式一 将其转换为数组
+     *
+     * @param str
+     * @param start
+     * @param end
+     * @return
+     */
+    public String reverse(String str, int start, int end){
+
+        if(str != null && str.length() > 0){
+
+            char[] arr = str.toCharArray();
+
+            for (int x =start,y = end; x < y; x++, y--){
+
+                char temp = arr[x];
+                arr[x] = arr[y];
+                arr[y] = temp;
+            }
+            return new String(arr);
+        }
+
+        return null;
+    }
+
+    /**
+     * 方式二 使用String 拼接
+     *
+     * @param str
+     * @param start
+     * @param end
+     * @return
+     */
+    public String reverse2(String str, int start, int end){
+
+        String reStr = str.substring(0, start);
+
+        for (int i = end; end >= start; i--){
+            reStr += str.charAt(i);
+        }
+        reStr += str.substring(end + 1);
+        return reStr;
+    }
+
+}
